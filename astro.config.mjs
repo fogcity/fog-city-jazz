f53f340a78d43715f48cbe60ffc2ecae0032534f
Matt Nichols <mattnichols206@gmail.com>
2025-07-05 16:49:10 -0700

    Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
 import decapCmsOauth from 'astro-decap-cms-oauth';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fogcityjazz.com',
+  output: 'server',
+  adapter: vercel(),
   integrations: [mdx(), sitemap(), tailwind(), decapCmsOauth()]
 });
