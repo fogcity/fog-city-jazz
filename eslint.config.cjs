784acb09f7e0214425f75af326555287d14bdef2
Matt Nichols <mattnichols206@gmail.com>
2025-07-06 17:35:14 -0700

    Fix Decap widget and lint config

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,3 +1,7 @@
 const { FlatCompat } = require('@eslint/eslintrc');
-const compat = new FlatCompat();
+const js = require('@eslint/js');
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
 module.exports = compat.config(require('./.eslintrc.cjs'));
