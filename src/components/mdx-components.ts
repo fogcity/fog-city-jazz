917857eb855a16d52f10e8c751d4eac84125eabe
Matt Nichols <mattnichols206@gmail.com>
2025-07-12 12:50:58 -0700

    feat: share mdx components

---
+++ src/components/mdx-components.ts
@@ -0,0 +1,9 @@
+import ImageTextBlock from './ImageTextBlock.astro';
+import SoundCloudEmbed from './SoundCloudEmbed.astro';
+
+export const mdxComponents = {
+  ImageTextBlock,
+  SoundCloudEmbed,
+};
+
+export default mdxComponents;
